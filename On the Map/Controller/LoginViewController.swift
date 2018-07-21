d06616f89bab9cee074b54108caf0d6161a9690b
David Rodrigues <davidnpr@hotmail.com>
2018-07-21 11:24:37 -0300

    Criação da tela de Login com seus outlets e actions.

---
+++ On the Map/Controller/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by David Rodrigues on 19/07/2018.
+//  Copyright © 2018 David Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func LogIn(_ sender: UIButton) {
+    }
+    
+    @IBAction func SignUp(_ sender: UIButton) {
+    }
+    
+}
